f7aa6852c3704b3f2550a828fecc72878d791d56
Mehdi <msoli666@gmail.com>
2018-11-06 19:31:19 +0330

    Add PHPDoc to Initial_Value class

--- includes/config/class-initial-value.php
+++ includes/config/class-initial-value.php
@@ -1,8 +1,38 @@
 <?php
+/**
+ * Initial_Value Class File
+ *
+ * Role of this class is like RC configuration files in application. If you need
+ * to initial value to start your plugin or need them for each time that WordPress
+ * run your plugin, you can use from this class.
+ *
+ * @package    Plugin_Name_Dir\Includes\Config
+ * @author     Your_Name <youremail@nomail.com>
+ * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @link       https://yoursite.com
+ * @since      1.0.0
+ */
 
 namespace Plugin_Name_Dir\Includes\Config;
+
+/**
+ * Class Initial_Value.
+ * If you need to initial value to start your plugin or need them for
+ * each time that WordPress run your plugin, you can use from this class.
+ *
+ * @package    Plugin_Name_Dir\Includes\Config
+ * @author     Your_Name <youremail@nomail.com>
+ */
 class Initial_Value {
 
+	/**
+	 * Initial values to create admin menu page.
+	 *
+	 * @access public
+	 * @static
+	 * @see    Includes/Admin/Admin_Menu
+	 * @return array It returns all of arguments that add_menu_page function needs.
+	 */
 	public static function sample_menu_page() {
 		$initial_value = [
 			'page_title'        => 'Sample Title',
@@ -17,6 +47,14 @@ class Initial_Value {
 		return $initial_value;
 	}
 
+	/**
+	 * Initial values to create admin submenu page (submenu1).
+	 *
+	 * @access public
+	 * @static
+	 * @see    Includes/Admin/Admin_Sub_Menu
+	 * @return array It returns all of arguments that add_submenu_page function needs.
+	 */
 	public static function sample_sub_menu_page1() {
 		$initial_value = [
 			'parent-slug'       => 'plugin-prefix-sample-url',
@@ -30,6 +68,14 @@ class Initial_Value {
 		return $initial_value;
 	}
 
+	/**
+	 * Initial values to create admin submenu page (submenu2).
+	 *
+	 * @access public
+	 * @static
+	 * @see    Includes/Admin/Admin_Sub_Menu
+	 * @return array It returns all of arguments that add_submenu_page function needs.
+	 */
 	public static function sample_sub_menu_page2() {
 		$initial_value = [
 			'parent-slug'       => 'plugin-prefix-sample-url',
@@ -42,5 +88,4 @@ class Initial_Value {
 
 		return $initial_value;
 	}
-
 }
