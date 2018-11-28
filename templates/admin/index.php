3d7381f8bf081e8370b71958f2bd27745e078e1a
Mehdi <msoli666@gmail.com>
2018-11-28 21:43:47 +0330

    Some minor changes in codes

---
+++ templates/admin/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The file is primary file to render admin page
+ *
+ * You can use from this file to define page (or pages) to render admin
+ * panels in admin panel of WordPress (or in specific management panel for your site)
+ *
+ * @package    Plugin_Name_Dir\templates\admin
+ * @author     Your_Name <youremail@nomail.com>
+ * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @link       https://yoursite.com
+ * @since      1.0.0
+ */
